e3f004e51f016f0a5d5b2c3a22f244ee37d5ef86
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-07-03 12:18:56 +0100

    West can step and turnRight

--- src/Orientations/West.js
+++ src/Orientations/West.js
@@ -1,8 +1,15 @@
 import North from "./North";
 
 export default class West {
+    constructor(initialCoordinates) {
+        this.currentCoordinates = initialCoordinates || new Coordinates(0,0)        
+    }
     
     turnRight() {
         return new North()
+    }
+
+    step(n = 1) {
+        return this.currentCoordinates.translate(n*-1, 0)
     }
 }
--- src/Orientations/West.test.js
+++ src/Orientations/West.test.js
@@ -1,12 +1,20 @@
 import West from './West'
 import North from './North';
+import Coordinates from '../Coordinates'
+
 
 describe('When facing West', () => {
-    let west = new West()
+    let currentCoordinates = new Coordinates(0,0)
+    let west = new West(currentCoordinates)
+
+    describe('and stepping', () =>  {
+        test('coordinates will be [-1,0]', () => {
+            expect(west.step()).toEqual(new Coordinates(-1,0))
+        })
+    })
     describe('and turning right', () => {        
         test('will be facing North', () => {
             expect(west.turnRight()).toBeInstanceOf(North)
         })
-
     })
 })
